03e1ff8f1701713cd6ef194a3719c191087fe7ca
muraligit2000 <119148214+muraligit2000@users.noreply.github.com>
2022-11-28 16:32:42 +0000

    Update HomeController.cs
    dsfsdfdsfds

--- HSBC.Deposits.Personnel.Vehicle/HSBC.Deposits.Personnel.Vehicle/Controllers/HomeController.cs
+++ HSBC.Deposits.Personnel.Vehicle/HSBC.Deposits.Personnel.Vehicle/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace HSBC.Deposits.Personnel.Vehicle.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            List<string> arrNames = new List<string>() { "Murali V", "Hari V" };
+            List<string> arrNames = new List<string>() { "Murali VV", "Hari VV" };
             return StatusCode(StatusCodes.Status200OK, arrNames);
         }
 
